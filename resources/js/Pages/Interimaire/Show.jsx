14ca6cbb834d0b4419abb7060be24096b50fb6bd
Bibadel <nicolasscharre@outlook.fr>
2023-03-19 20:30:42 +0100

    [ Front End | Interimaire Show ] Mise à jour contrat

--- resources/js/Pages/Interimaire/Show.jsx
+++ resources/js/Pages/Interimaire/Show.jsx
@@ -1,8 +1,33 @@
 import {Link} from "@inertiajs/react";
 import Header from "../../Components/Header";
-import ApplicationButton from '@/Components/Navigation/ApplicationButton'
+import ApplicationButton from '@/Components/Navigation/ApplicationButton';
+import {Inertia} from '@inertiajs/inertia';
 
 export default function Show(props) {
+    console.log(props)
+    let creationDate = new Date(props.interimaire.created_at);
+    creationDate = creationDate.toLocaleDateString('fr-FR', {year: 'numeric', month: 'long', day: 'numeric'});
+
+    const contratData = {
+        color: "red",
+        text: "Aucun contrats n'a été signé",
+        action: "Signer un contrat",
+    }
+    
+    if(props.contrat !== null){
+        contratData.action = null,
+        contratData.color = "green",
+        contratData.text = "Contrat signé"
+    }
+
+    const handleClickContrat = (e) => {
+        e.preventDefault();
+        if(contratData.action !== null){
+            Inertia.get(route('contrat.create'))
+        }else{
+            Inertia.get(route('contrat.show', props.contrat))
+        }
+    }   
     return (
         <>
         <Header title={props.interimaire.firstname} url={props.ziggy.url} />
@@ -23,11 +48,11 @@ export default function Show(props) {
                 </div>
 
 
-                <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
+                <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6" >
+                    <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg" onClick={handleClickContrat}>
                         <h3 class="text-lg font-semibold text-gray-800"><b><u>État du contrat :</u></b></h3>
-                        <p class="mt-2 text-green-600">Signé par les deux parties</p>
-                        <p className="mt-2"> Fais le 02/03/5025</p>
+                        <p class={`mt-2 text-${contratData.color}-600`}>{contratData.text}</p>
+                        { contratData.action !== null ? "Appuyez pour créer un contrat" : ""}
                         <div class="absolute inset-0 rounded-lg bg-gradient-to-br from-blue-500 to-indigo-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
                     </div>
                     <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
@@ -37,7 +62,7 @@ export default function Show(props) {
                     </div>
                     <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
                         <h3 class="text-lg font-semibold text-gray-800"><b> <u>Inscris depuis  : </u></b></h3>
-                        <p class="mt-2 text-gray-600"><i>02/05/2022</i></p>
+                        <p class="mt-2 text-gray-600"><i>{creationDate}</i></p>
                         <div class="absolute inset-0 rounded-lg bg-gradient-to-br from-green-500 to-teal-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
                     </div>
                 </div>
