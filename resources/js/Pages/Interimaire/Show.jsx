9a4167c2cb5696830624d3f0d90c0336e591c9dd
Bibadel <nicolasscharre@outlook.fr>
2023-03-19 23:02:40 +0100

    [Front-End | Intérimaire ] Show, Mise à jour de la mise en page contrat

--- resources/js/Pages/Interimaire/Show.jsx
+++ resources/js/Pages/Interimaire/Show.jsx
@@ -31,39 +31,39 @@ export default function Show(props) {
     return (
         <>
         <Header title={props.interimaire.firstname} url={props.ziggy.url} />
-        <div class="container mx-auto px-4 py-12">
-            <div class="bg-white p-6 rounded-lg shadow-md">
+        <div className="container mx-auto px-4 py-12">
+            <div className="bg-white p-6 rounded-lg shadow-md">
                 <div className="absolute pt-0" >
                     <Link href={route('interimaire.index')}>⬅ Retour à la liste des intérimaires </Link>
                 </div>
-                <div class="flex items-center justify-center">
-                    <div class="w-24 h-24 relative">
-                        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/worker-wearing-5379423-4495968.png" alt="Avatar" class="w-full h-full object-cover rounded-full border-4 border-blue-500" />
-                        <div class="absolute -bottom-2 -right-2 w-8 h-8 bg-green-500 border-2 border-white rounded-full"></div>
+                <div className="flex items-center justify-center">
+                    <div className="w-24 h-24 relative">
+                        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/worker-wearing-5379423-4495968.png" alt="Avatar" className="w-full h-full object-cover rounded-full border-4 border-blue-500" />
+                        <div className="absolute -bottom-2 -right-2 w-8 h-8 bg-green-500 border-2 border-white rounded-full"></div>
                     </div>
-                    <div class="ml-4">
-                        <h2 class="text-xl font-semibold text-gray-800">{props.interimaire.firstname + " " + props.interimaire.lastname}</h2>
+                    <div className="ml-4">
+                        <h2 className="text-xl font-semibold text-gray-800">{props.interimaire.firstname + " " + props.interimaire.lastname}</h2>
                     
                     </div>
                 </div>
 
 
-                <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6" >
-                    <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg" onClick={handleClickContrat}>
-                        <h3 class="text-lg font-semibold text-gray-800"><b><u>État du contrat :</u></b></h3>
-                        <p class={`mt-2 text-${contratData.color}-600`}>{contratData.text}</p>
+                <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6" >
+                    <div className={`relative p-6 rounded-lg shadow-md bg-opacity-20 bg-${contratData.color}-600 transition-all duration-200 hover:shadow-lg`} onClick={handleClickContrat}>
+                        <h3 className="text-lg font-semibold text-gray-800"><b><u>État du contrat :</u></b></h3>
+                        <p className="mt-2 text-gray-600">{contratData.text}</p>
                         { contratData.action !== null ? "Appuyez pour créer un contrat" : ""}
-                        <div class="absolute inset-0 rounded-lg bg-gradient-to-br from-blue-500 to-indigo-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
+                        <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-blue-500 to-indigo-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
                     </div>
-                    <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
-                        <h3 class="text-lg font-semibold text-gray-800"><b> <u>Nombres de trajets faits : </u></b></h3>
-                        <p class="mt-2 text-gray-600"><i>{ props.trajets.length}</i></p>
-                        <div class="absolute inset-0 rounded-lg bg-gradient-to-br from-green-500 to-teal-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
+                    <div className="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
+                        <h3 className="text-lg font-semibold text-gray-800"><b> <u>Nombres de trajets faits : </u></b></h3>
+                        <p className="mt-2 text-gray-600 "><i>{ props.trajets.length}</i></p>
+                        <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-green-500 to-teal-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
                     </div>
-                    <div class="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
-                        <h3 class="text-lg font-semibold text-gray-800"><b> <u>Inscris depuis  : </u></b></h3>
-                        <p class="mt-2 text-gray-600"><i>{creationDate}</i></p>
-                        <div class="absolute inset-0 rounded-lg bg-gradient-to-br from-green-500 to-teal-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
+                    <div className="relative p-6 rounded-lg shadow-md transition-all duration-200 hover:shadow-lg">
+                        <h3 className="text-lg font-semibold text-gray-800"><b> <u>Inscris depuis  : </u></b></h3>
+                        <p className="mt-2 text-gray-600"><i>{creationDate}</i></p>
+                        <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-green-500 to-teal-600 opacity-0 hover:opacity-40 transition-opacity duration-200"></div>
                     </div>
                 </div>
             </div>
